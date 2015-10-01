fc43470ce0d0e6b221e0a724fba2eb826032f9ce
Mark Ormesher <me@markormesher.co.uk>
2015-10-01 13:13:03 +0100

    Documented styles.xml

--- app/src/main/java/tech/kcl/kcltechtodo/MainActivity.java
+++ app/src/main/java/tech/kcl/kcltechtodo/MainActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
 }
