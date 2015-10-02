9703847dbd4e2101dea44bf7dc1cd47aba929c75
Mark Ormesher <me@markormesher.co.uk>
2015-10-02 06:37:23 +0100

    Better documentation for Activity

--- app/src/main/java/tech/kcl/kcltechtodo/HelloWorldActivity.java
+++ app/src/main/java/tech/kcl/kcltechtodo/HelloWorldActivity.java
@@ -14,7 +14,23 @@ can handle user interactions, and your user can navigate between them.
 
 Activities have a fairly complex lifecycle that they travel through as
 they are created and destroyed. We will cover the four main events that
-an Activity can go through: create, resume, pause and stop.
+an Activity can go through in the lecture, but here's a summary:
+
+onCreate: the activity has been created for the first time, but may not
+          be visible to the user yet
+
+onResume: the activity is now "on top" and can be seen by the user
+
+onPause:  the activity has been obscured by something (another app
+          opening, the user pressing "Home", a phone call, etc.)
+
+onStop:   the activity has been stopped and will terminate completely
+
+The most important thing to know about the lifecycle is that an activity
+can exist in the background without being fully closed. For example, a
+user is using your app and then receives a phone call - the call covers
+your activity, but it isn't fully closed; it's "paused". It will be
+"resumed" when their call ends.
 
 ======================================================================*/
 
