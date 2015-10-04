ba1d6648512963286cefb9d6c43ffa8b9bd331e0
AnanaMJ <ana.jalba13@gmail.com>
2015-10-04 15:12:39 +0100

    Add plus button to action bar

--- app/src/main/java/tech/kcl/kcltechtodo/TaskListActivity.java
+++ app/src/main/java/tech/kcl/kcltechtodo/TaskListActivity.java
@@ -2,6 +2,10 @@ package tech.kcl.kcltechtodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class TaskListActivity extends AppCompatActivity {
 
@@ -16,4 +20,22 @@ public class TaskListActivity extends AppCompatActivity {
 		setTitle(getString(R.string.task_list_activity_title));
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.task_list_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+			case R.id.create_task:
+				Toast.makeText(getApplicationContext(), "This works!", Toast.LENGTH_LONG).show();
+				//TODO: send to create activity
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
